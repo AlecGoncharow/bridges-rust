134a3e62b7f15e0acd9088259a169922ec381625
Alec Goncharow <Alec.Goncharow@gmail.com>
2018-11-24 18:33:41 -0500

    Added head and tail to LinkedList

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,5 +1,7 @@
 use super::element::Element;
 use super::link::Link;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Serialize, Deserialize)]
 pub struct LinkedList<T: Default> {
@@ -7,6 +9,10 @@ pub struct LinkedList<T: Default> {
     pub dims: Vec<i8>,
     nodes: Vec<Element<T>>,
     links: Vec<Link>,
+    #[serde(skip_serializing, skip_deserializing)]
+    head: Option<Rc<RefCell<Element<T>>>>,
+    #[serde(skip_serializing, skip_deserializing)]
+    tail: Option<Rc<RefCell<Element<T>>>>,
 }
 
 impl<T: Default> LinkedList<T> {
@@ -16,6 +22,8 @@ impl<T: Default> LinkedList<T> {
             dims: vec![0, 0, 0],
             nodes: vec![],
             links: vec![],
+            head: None,
+            tail: None,
         }
     }
 }
